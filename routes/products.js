753a241a846692c7f4038c47762c36344bb069bf
jksun <jksun@thoughtworks.com>
2014-08-03 22:05:54 +0800

    return 200 when get product by id

--- routes/products.js
+++ routes/products.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res) {
     return res.send(200);
 });
 
+router.get('/:id', function (req, res) {
+    return res.send(200);
+});
+
 module.exports = router;
--- test/product_test.js
+++ test/product_test.js
@@ -4,12 +4,26 @@ var request = require("supertest");
 var app = require("../app");
 
 describe("Product", function () {
-    it("get", function (done) {
-        request(app).get("/products").expect(200).end(function(err, result) {
-            if(err) {
-                return done(err);
-            }
-            done();
+    describe("Get products", function () {
+        it("get", function (done) {
+            request(app).get("/products").expect(200).end(function (err, result) {
+                if (err) {
+                    return done(err);
+                }
+                done();
+            });
+        });
+    });
+
+
+    describe("Get product", function () {
+        it("get by id", function (done) {
+            request(app).get("/products/1").expect(200).end(function (err, result) {
+                if (err) {
+                    return done(err);
+                }
+                done();
+            });
         });
     });
 });
